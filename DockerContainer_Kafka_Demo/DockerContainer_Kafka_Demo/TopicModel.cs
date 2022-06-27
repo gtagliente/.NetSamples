84205c856f16059da6e346d77d809a5f4bf5eae0
Gianluca <gianluca.profile@gmail.com>
2022-06-27 08:05:12 +0200

    New Topic Model

---
+++ DockerContainer_Kafka_Demo/DockerContainer_Kafka_Demo/TopicModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockerContainer_Kafka_Demo
+{
+    public class TopicModel
+    {
+        public string TopicName { get; set; }
+
+        public string TopicDescription { get; set; }
+    }
+}
